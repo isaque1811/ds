26d10fc721a9b3cf1bf13f6f6ad3a6015d9344f9
isaque1811 <144157112+isaque1811@users.noreply.github.com>
2023-10-17 14:14:53 -0300

    continuaçao

--- atv12.py
+++ atv12.py
@@ -1,3 +1,3 @@
 num1=float(input("qual sua altura?"))
-num2=72.7*altura-58
+num2=72.7*num1-58
 print('o seu peso medio é',num2)
---
+++ atv14.py
@@ -0,0 +1,4 @@
+num1=float(input("escreva seu peso"))
+num2=num1-50
+num3=num2*4
+print('em excesso foram {}kg a multa é {}'.format(num2,num3))
