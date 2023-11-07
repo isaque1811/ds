db6b7d680ad3277184a8dd67704135bec1414e49
isaque <wwwisaque18@gmail.com>
2023-11-07 14:07:41 -0300

    atv2
    continuaçao da lista 2

---
+++ atv20.py
@@ -0,0 +1,5 @@
+s =int(input("digite um numero "))
+if s < 0:
+    print('esse  numero é positivo')
+elif s <0 :
+    print('esse numero é negativo')
---
+++ atv22.py
@@ -0,0 +1,11 @@
+l=input('digite uma letra')
+if (
+    l == "a" 
+    or l=="e" 
+    or l=="i" 
+    or l=="o" 
+    or l=="u" 
+    ):
+    print('é uma vogal')
+else:
+    print('nao é uma vogal')
